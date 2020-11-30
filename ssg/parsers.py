deccd4b000dc32bfca5238db5e08040a47bd107b
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-11-30 14:42:18 -0500

    fix open parameters

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,7 +11,7 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path):
-        with open(path) as file:
+        with open(path, "r") as file:
             return read(file)
 
             
