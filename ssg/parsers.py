4d98847b5b4e52694a7fa33ae5ab0fc612d6cff5
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-11-30 14:50:23 -0500

    add parser write()

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -14,6 +14,8 @@ class Parser:
         with open(path, "r") as file:
             return file.read()
 
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
             
 
 
