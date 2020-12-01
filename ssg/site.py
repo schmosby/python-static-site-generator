4f62da1796f3eee31d8e1d7c94693c52d13973a4
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-12-01 14:57:21 -0500

    step 9 - avaible parsers

--- ssg/site.py
+++ ssg/site.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers = "none"):
         self.source = Path(source)
         self.dest = Path(dest)
-
+        self.parsers =  parsers or []
+        
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
