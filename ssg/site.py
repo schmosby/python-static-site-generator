885ed6142853eda2a15ab04724de2ef19f3e021f
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-12-01 14:59:50 -0500

    none to None

--- ssg/site.py
+++ ssg/site.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest, parsers = none):
+    def __init__(self, source, dest, parsers = None):
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers =  parsers or []
