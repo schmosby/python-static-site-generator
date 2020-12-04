b3276324b3e16db2426695840979d0c87c4ed225
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-12-04 14:42:59 -0500

    step 14 run parsedr

--- ssg/site.py
+++ ssg/site.py
@@ -15,6 +15,9 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+                
     def load_parser(self,extension):
         for parser in self.parsers:
             if parser.valid_extension(extension):
@@ -26,6 +29,7 @@ class Site:
             parser.parse(path, self.source, self.dest)
         else:
             print("Not Implemented")
+
         
 
 
