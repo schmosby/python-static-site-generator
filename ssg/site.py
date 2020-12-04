40e49fb76d79d5979b934027df2fdb35e0898ee7
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-12-04 09:02:44 -0500

    add self to load_parser()

--- ssg/site.py
+++ ssg/site.py
@@ -21,8 +21,8 @@ class Site:
                 return parser
 
     def run_parser(self,path):
-        parser = load_parser(path.suffix)
-        
+        parser = self.load_parser(path.suffix)
+
 
 
 
