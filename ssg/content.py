e21f6b217eae1fe81c7be12bf130dc83a0b65eb7
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-12-07 08:45:17 -0500

    step 5/6

--- ssg/content.py
+++ ssg/content.py
@@ -12,7 +12,17 @@ class Content(Mapping):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
-        
+
     def __init__(self, metadata, content):
         self.data = metadata
         self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    
