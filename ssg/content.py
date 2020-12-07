a113722ac0f73bb2bf0a12e50b3c85993a198a44
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-12-07 08:31:06 -0500

    step3: indentation matters :(

--- ssg/content.py
+++ ssg/content.py
@@ -7,8 +7,8 @@ class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
-@classmethod
-def load(cls, string):
+    @classmethod
+    def load(cls, string):
     _, fm, content = cls.__regex.split(string, 2)
     metadata = load(fm, Loader=FullLoader)
     return cls(metadata, content)
