a792874c120fe72154e0abdb929740272265b8e2
Schmosby <5651207+schmosby@users.noreply.github.com>
2020-12-08 11:09:56 -0500

    step 6 and 7

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -48,4 +48,13 @@ class MarkdownParser(Parser):
 
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
-    
+
+    def parse(self, path: Path,source: Path,dest: Path):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(["html_body"],dest,html)
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
+
+
